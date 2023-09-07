7cfa33c161e9cfe83b4572e5b7def255ee1a1ee3
phdljr <phdljr@naver.com>
2023-09-08 03:45:56 +0900

    feat: add skeleton code

---
+++ src/components/ArticleList.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Article from "./Article.svelte";
+  import ArticleLoading from "./ArticleLoading.svelte";
+</script>
+
+<div class="slog-list-wrap">
+  <ul class="slog-ul">
+    <li class="mb-5">
+      <Article />
+    </li>
+  </ul>
+</div>
