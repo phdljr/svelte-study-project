8523e28ebfb9555ae5315e10dbbb54129eb7b935
phdljr <phdljr@naver.com>
2023-09-09 06:36:59 +0900

    feat: add scroll pagination

--- src/stores/index.js
+++ src/stores/index.js
@@ -3,9 +3,118 @@ import { writable, get, derived } from "svelte/store";
 import { getApi, postApi, putApi, delApi } from "../service/api";
 import { router } from "tinro";
 
-function setCurrentAtriclesPage() { }
-function setArticles() { }
-function setLoadingArticle() { }
+// 무한 스크롤 기반 페이지네이션
+function setCurrentArticlesPage() {
+    // 초기 페이지 값을 1로 설정
+    const { subscribe, update, set } = writable(1);
+
+    const resetPage = () => set(1);
+
+    // 페이지가 1 증가할 때 마다, 데이터도 불러온다.
+    const increPage = () => {
+        update(data => data = data + 1);
+        articles.fetchArticles();
+    };
+
+    return {
+        subscribe,
+        resetPage,
+        increPage,
+    }
+}
+
+// 글 목록 가져오는 기능
+function setArticles() {
+    let initValues = {
+        articleList: [],
+        totalPageCount: 0,
+        menuPopup: "",
+        editMode: "",
+    };
+
+    const { subscribe, update, set } = writable({ ...initValues });
+
+    const fetchArticles = async () => {
+        // 페이지 로딩 시, 추가 로딩을 잠금
+        loadingArticle.turnOnLoading();
+
+        // get(): 다른 스토어 또는 일반 js 파일에서 스벨트의 스토어값을 가져오는 함수
+        // $ 기호는 js 파일에선 사용하지 못한다.
+        const currentPage = get(currentArticlesPage);
+        let path = `/articles/?pageNumber=${currentPage}`;
+
+        try {
+            // $ 기호는 js 파일에선 사용하지 못한다. 그래서 get을 통해 가져온다.
+            const access_token = get(auth).Authorization;
+
+            const options = {
+                path: path,
+                access_token: access_token,
+            };
+
+            const getDatas = await getApi(options);
+
+            const newData = {
+                articleList: getDatas.articleList,
+                totalPageCount: getDatas.totalPageCount,
+            };
+
+            update(datas => {
+                // 첫 페이지라면 받아온 데이터를 그대로 대입한다.
+                if (currentPage === 1) {
+                    datas.articleList = newData.articleList;
+                    datas.totalPageCount = newData.totalPageCount;
+                } else { // 첫 페이지가 아니라면, 스프레드를 시켜 이전 데이터에 연결시킨다.
+                    const newArticles = [...datas.articleList, ...newData.articleList];
+                    datas.articleList = newArticles;
+                    datas.totalPageCount = newData.totalPageCount;
+                }
+
+                return datas;
+            })
+
+            // 페이지 로딩이 다 됐을 시, 추가 로딩 잠금 해제
+            loadingArticle.turnOffLoading();
+        } catch (error) {
+            // 오류 발생 시에도, 더 이상 나타나지 않게 함
+            loadingArticle.turnOffLoading();
+            throw error;
+        }
+    };
+    const resetArticles = () => {
+        set({ ...initValues });
+        currentArticlesPage.resetPage();
+        // 페이지 초기화 시, 잠금 해제
+        articlePageLock.set(false);
+    };
+
+    return {
+        subscribe,
+        fetchArticles,
+        resetArticles,
+    }
+}
+function setLoadingArticle() {
+    const { subscribe, set } = writable(false);
+
+    // 데이터를 가져올 때, 잠금
+    const turnOnLoading = () => {
+        set(true);
+        articlePageLock.set(true);
+    }
+
+    // 데이터를 가져왔을 때, 잠금 해제
+    const turnOffLoading = () => {
+        set(false);
+        articlePageLock.set(false);
+    }
+
+    return {
+        subscribe,
+        turnOnLoading,
+        turnOffLoading,
+    }
+}
 function setAtricleContent() { }
 function setComments() { }
 function setAuth() {
@@ -101,8 +210,9 @@ function setIsLogin() {
     return checkLogin;
 }
 
-export const currentAtriclesPage = setCurrentAtriclesPage();
+export const currentArticlesPage = setCurrentArticlesPage();
 export const articles = setArticles();
+export const articlePageLock = writable(false);
 export const loadingArticle = setLoadingArticle();
 export const articleContent = setAtricleContent();
 export const comments = setComments();
