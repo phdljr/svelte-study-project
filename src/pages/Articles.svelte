04654574ceee761d5b78d6071ccccb9b7041ba83
phdljr <phdljr@naver.com>
2023-09-11 04:16:05 +0900

    edit: check type to false

--- src/pages/Articles.svelte
+++ src/pages/Articles.svelte
@@ -1,6 +1,4 @@
 <script>
-  // @ts-nocheck
-
   import ArticleHeader from "../components/ArticleHeader.svelte";
   import ArticleList from "../components/ArticleList.svelte";
   import ArticleAddForm from "../components/ArticleAddForm.svelte";
